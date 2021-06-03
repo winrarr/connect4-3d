a55bca0451f126ea22e732a36f32fa46db4697f5
KT <au637855@uni.au.dk>
2021-06-03 13:28:00 +0200

    added colors when hovering a rod with the cursor TODO: make create colors take from world as in color_rods

--- src/board.rs
+++ src/board.rs
@@ -1,5 +1,5 @@
 use bevy::{math::f32, prelude::*};
-use bevy_mod_picking::{PickableBundle, PickingPlugin, DebugCursorPickingPlugin};
+use bevy_mod_picking::*;
 
 const BOARD_HEIGHT: f32 = 0.2;
 const BOARD_SIZE: f32 = 4.;
@@ -24,7 +24,7 @@ fn create_board(
             min_z: 0.,
             max_z: BOARD_SIZE,
         })),
-        material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+        material: materials.add(Color::rgb_u8(130, 73, 11).into()),
         transform: Transform::from_xyz(0.0, 0.0, 0.0),
         ..Default::default()
     });
@@ -39,7 +39,7 @@ fn create_board(
         uv_profile: shape::CapsuleUvProfile::Uniform,
     }));
 
-    let rod_material: Handle<StandardMaterial> = materials.add(Color::rgb(0.8, 0.7, 0.6).into());
+    let rod_material: Handle<StandardMaterial> = materials.add(Color::rgb_u8(130, 73, 11).into());
 
     for x in 0u8..4 {
         for y in 0u8..4 {
@@ -51,6 +51,38 @@ fn create_board(
 pub struct Rod {
     pub x: f32,
     pub y: f32,
+}
+
+fn color_rods(
+    selected_rod: Res<SelectedRod>,
+    materials: Res<RodMaterials>,
+    mut query: Query<(Entity, &Rod, &mut Handle<StandardMaterial>)>,
+    picking_camera_query: Query<&PickingCamera>,
+) {
+    // Get entity under the cursor, if there is one
+    let top_entity = match picking_camera_query.iter().last() {
+        Some(picking_camera) => match picking_camera.intersect_top() {
+            Some((entity, _intersection)) => Some(entity),
+            None => None,
+        },
+        None => None,
+    };
+
+    for (entity, _, mut material) in query.iter_mut() {
+        // Change the material
+        *material = if Some(entity) == top_entity {
+            materials.highlight_color.clone()
+        } else if Some(entity) == selected_rod.entity {
+            materials.selected_color.clone()
+        } else {
+            materials.base_color.clone()
+        };
+    }
+}
+
+#[derive(Default)]
+struct SelectedRod {
+    entity: Option<Entity>,
 }
 
 fn spawn_rod(
@@ -69,12 +101,35 @@ fn spawn_rod(
     .insert(Rod { x: position.0, y: position.1 });
 }
 
+struct RodMaterials {
+    base_color: Handle<StandardMaterial>,
+    highlight_color: Handle<StandardMaterial>,
+    selected_color: Handle<StandardMaterial>,
+}
+
+impl FromWorld for RodMaterials {
+    fn from_world(world: &mut World) -> Self {
+        let world = world.cell();
+        let mut materials = world
+            .get_resource_mut::<Assets<StandardMaterial>>()
+            .unwrap();
+        RodMaterials {
+            base_color: materials.add(Color::rgb_u8(130, 73, 11).into()),
+            highlight_color: materials.add(Color::rgb(0.8, 0.3, 0.3).into()),
+            selected_color: materials.add(Color::rgb(0.9, 0.1, 0.1).into()),
+        }
+    }
+}
+
 pub struct BoardPlugin;
 impl Plugin for BoardPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app
+            .init_resource::<SelectedRod>()
+            .init_resource::<RodMaterials>()
             .add_plugin(PickingPlugin)
             .add_plugin(DebugCursorPickingPlugin)
-            .add_startup_system(create_board.system());
+            .add_startup_system(create_board.system())
+            .add_system(color_rods.system());
     }
 }
