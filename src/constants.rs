b5c23c4aed96beef330157e49ff70de5bad50cc6
KT <au637855@uni.au.dk>
2021-07-01 17:44:02 +0200

    added constants.rs to git as well :P

---
+++ src/constants.rs
@@ -0,0 +1,9 @@
+pub const BOARD_HEIGHT: f32 = 0.2;
+pub const BOARD_SIZE: f32 = 4.;
+
+pub const ROD_HEIGHT: f32 = 0.5;
+pub const ROD_RADIUS: f32 = 0.07;
+pub const SPACE: f32 = BOARD_SIZE / 4.;
+pub const OFFSET: f32 = SPACE / 2.;
+
+pub const PIG_RADIUS: f32 = 0.1;
